63dd050a51ae1c552487b2329c72095e68072e5c
Brando Bautista <al25220409@utcj.edu.mx>
2026-06-23 11:51:00 -0600

    Actualizar mensaje para CD con Docker

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Calculadora funcionando con Docker"
+    return "Calculadora actualizada con Docker y despliegue continuo"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
